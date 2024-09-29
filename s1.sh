5a38e1ab555374f5c043011424db540a225c613d
Shashi1408 <47396633+Shashi1408@users.noreply.github.com>
2024-09-29 01:00:23 -0700

    Update s1.sh

--- s1.sh
+++ s1.sh
@@ -31,5 +31,5 @@ mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
-sleep 30
+sleep 60
 kubectl apply -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
