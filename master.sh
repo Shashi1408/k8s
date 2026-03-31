44ab1a53d3e3c637d69f30a1988dda7c3f25efe6
Shashi1408 <47396633+Shashi1408@users.noreply.github.com>
2026-03-31 17:36:40 +0530

    Update master.sh

--- master.sh
+++ master.sh
@@ -5,7 +5,7 @@ PRIVATE_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 
 # 2. Initialize Cluster
 # Note: --pod-network-cidr=192.168.0.0/16 is the default for Calico
-sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address=$PRIVATE_IP
+sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address=$PRIVATE_IP --ignore-preflight-errors=FileExisting-conntrack
 
 # 3. Setup Kubeconfig for current user
 mkdir -p $HOME/.kube
